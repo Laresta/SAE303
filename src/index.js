9c62c49c64a9c86bfcd800f14326ff233504ce6f
bryan duarte <duartebryan@hotmail.fr>
2022-10-14 09:15:44 +0200

    yeeeeeeeeeee v1 moche

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 $(".nav-item").css("margin-left","1.5em")
+
+let recherche = "#carrousel>ul>li"
+
+let imgs = $(recherche)
+
+
+function carprec() {
+  let idimgact = parseInt($(".imgactive").attr("id"))
+  console.log(idimgact);
+  let imgprec = idimgact - 1
+  if (idimgact === 0) {
+    imgprec = 3
+  }
+  console.log(imgprec);
+  imgs.each(function(){
+    if (parseInt($(this).attr("id")) === imgprec ){
+      $(this).toggleClass('imgactive')
+      $("#"+idimgact).toggleClass("imgactive")
+    }
+  })
+}
+
+function carsuiv() {
+  console.log("ahhhhh");
+  let idimgact = parseInt($(".imgactive").attr("id"))
+  let imgsuiv = idimgact + 1
+  if (idimgact === 3) {
+    imgsuiv = 0
+  }
+  console.log(imgsuiv);
+  imgs.each(function(){
+    if (parseInt($(this).attr("id")) === imgsuiv ){
+      $(this).toggleClass('imgactive')
+      $("#"+idimgact).toggleClass("imgactive")
+    }
+    }
+  )
+}
